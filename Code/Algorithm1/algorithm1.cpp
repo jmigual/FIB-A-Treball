10bcfe80707b40debaf776d68eef988e0bc06965
jmigual <j.marce.igual@gmail.com>
2016-05-12 15:25:45 +0200

    Added Makefile and basic code

---
+++ Code/Algorithm1/algorithm1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../typedefs.h"
+using namespace std;
+
+void algorithm(VVC &dictionary, VVC &alphabetSoup) {
+    cout << "Prova" << endl;
+}
---
+++ Code/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "typedefs.h"
+using namespace std;
+
+int main()
+{
+    VVC caca;
+    algorithm(caca, caca);
+}
+
