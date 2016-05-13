76d0bcb125d0f912dc380853656bbc2c2ddac721
jmigual <j.marce.igual@gmail.com>
2016-05-13 13:49:24 +0200

    Added readinput option

--- Code/main.cpp
+++ Code/main.cpp
@@ -4,9 +4,41 @@ using namespace std;
 
 int main()
 {
-    VVC caca;
-    algorithm(caca, caca);
-    
-    // He afegit un comentari
+    // Read dictionary
+    int n;
+    cin >> n;
+
+    VVC dictionary(n);
+    for (VC &V : dictionary) {
+        int m;
+        cin >> m;
+        V.resize(n);
+
+        for (char &c: V) {
+            cin >> c;
+            c -= '0';
+
+            if (c > 9) {
+                cout << "Error! wrong symbol: " << c + '0';
+                return 1;
+            }
+        }
+    }
+
+    // Read soup
+    VVC alphabetSoup(n, VC(n));
+    for (VC &V : alphabetSoup) {
+        for (char &c : V) {
+            cin >> c;
+            c -= '0';
+
+            if (c > 9) {
+                cout << "Error! wrong symbol: " << c + '0';
+                return 1;
+            }
+        }
+    }
+
+    algorithm(dictionary, alphabetSoup);
 }
 
--- Code/typedefs.h
+++ Code/typedefs.h
@@ -3,6 +3,9 @@
 
 #include <vector>
 using namespace std;
+
+typedef unsigned int uint;
+
 typedef vector< char > VC;
 typedef vector< VC >   VVC;
 
