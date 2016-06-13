4156e2a3a43e953056e88905cd3fb136735ef113
jmigual <j.marce.igual@gmail.com>
2016-06-13 21:11:52 +0200

    Moved all files to folders

---
+++ Code/dictionaries/hashdictionary.cpp
@@ -0,0 +1,26 @@
+#include "hashdictionary.h"
+
+HashDictionary::HashDictionary() {
+
+}
+
+void HashDictionary::insertElement(const string &) {
+
+}
+
+bool HashDictionary::contains(const string &) {
+    return false;
+}
+
+void HashDictionary::reset() {
+
+}
+
+bool HashDictionary::stepForwards(char) {
+    return false;
+}
+
+bool HashDictionary::stepBackwards() {
+    return false;
+}
+
