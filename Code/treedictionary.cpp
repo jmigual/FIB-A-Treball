45fb26daffe3251d0cc0fd8fa95e84f8d2984095
jmigual <j.marce.igual@gmail.com>
2016-06-13 15:08:03 +0200

    Finished tree dictionary bugfixing starts now!

--- Code/treedictionary.cpp
+++ Code/treedictionary.cpp
@@ -1,20 +1,46 @@
 #include "treedictionary.h"
 
-void TreeDictionaryNode::insertElement(string element, uint index) {
+TreeDictionaryNode::~TreeDictionaryNode() {
+    for (auto it : m_nodes) {
+        delete it.second;
+    }
+}
+
+void TreeDictionaryNode::insertElement(const string &element, uint index) {
     auto it = m_nodes.find(element[index]);
+
+    if (it == m_nodes.end()) {
+        // Insert the element
+        pair<char, TreeDictionaryNode *> ins;
+        ins.first = element[index];
+        ins.second = new TreeDictionaryNode;
+        pair<ucTit, bool> p = m_nodes.insert(ins);
+        it = p.first;
+    }
+
+    if (element.length() > index + 1) {
+        it->second->insertElement(element, index + 1);
+    }
 }
 
 bool TreeDictionaryNode::contains(const string &element, uint index) {
-    if (index >= element.length()) return true;
-    
-    
+    auto it = m_nodes.find(element[index]);
+
+    if (it == m_nodes.end()) return false;
+    if (element.length() <= index + 1) return true;
+    return it->second->contains(element, index + 1);
 }
 
 TreeDictionaryNode *TreeDictionaryNode::getNode(char c) {
     auto it = m_nodes.find(c);
-    
+
     if (it == m_nodes.end()) return nullptr;
     return it->second;
 }
 
 
+
+TreeDictionary::TreeDictionary()
+        : m_rootNode(new TreeDictionaryNode) {
+
+}
--- Code/treedictionary.h
+++ Code/treedictionary.h
@@ -2,49 +2,50 @@
 #define TREEDICTIONARY_H
 #include <unordered_map>
 #include <string>
-#include <map>
 using namespace std;
 
 class TreeDictionaryNode
 {
+    typedef unordered_map<char, TreeDictionaryNode *>::iterator ucTit;
+
 public:
-    
-    TreeDictionaryNode();
-    
-    void insertElement(string element, uint index);
-    
-    bool contains(const string& element, uint index);
-    
-    TreeDictionaryNode* getNode(char c);
-    
+
+    ~TreeDictionaryNode();
+
+    void insertElement(const string &element, uint index);
+
+    bool contains(const string &element, uint index);
+
+    TreeDictionaryNode *getNode(char c);
+
 private:
-    
-    unordered_map<char, TreeDictionaryNode*> m_nodes;
+
+    unordered_map<char, TreeDictionaryNode *> m_nodes;
 };
 
 class TreeDictionary
 {
 public:
-    
-    inline void insertElement(const string& element) {
+
+    TreeDictionary();
+
+    inline void insertElement(const string &element) {
         if (element.length() == 0) return;
         m_rootNode->insertElement(element, 0);
     }
-    
-    inline bool contains(const string& element) {
+
+    inline bool contains(const string &element) {
         if (element.length() == 0) return false;
         m_rootNode->contains(element, 0);
     }
-    
-    inline TreeDictionaryNode* getNode(char c) {
+
+    inline TreeDictionaryNode *getNode(char c) {
         return m_rootNode->getNode(c);
     }
-    
+
 private:
-    
-    map<char, char> m_map;
-    
-    TreeDictionaryNode* m_rootNode;
+
+    TreeDictionaryNode *m_rootNode;
 };
 
 #endif // TREEDICTIONARY_H
