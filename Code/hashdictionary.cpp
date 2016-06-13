3c2d23f97914367c86f67ce07d7a1bc275c0ac42
jmigual <j.marce.igual@gmail.com>
2016-06-13 20:35:52 +0200

    Add minimum functions

--- Code/hashdictionary.cpp
+++ Code/hashdictionary.cpp
@@ -1,7 +1,26 @@
 #include "hashdictionary.h"
 
-HashDictionary::HashDictionary()
-{
-    
+HashDictionary::HashDictionary() {
+
+}
+
+void HashDictionary::insertElement(const string &elem) {
+
+}
+
+bool HashDictionary::contains(const string &elem) {
+
+}
+
+void HashDictionary::reset() {
+
+}
+
+bool HashDictionary::stepForwards(char c) {
+
+}
+
+bool HashDictionary::stepBackwards() {
+
 }
 
