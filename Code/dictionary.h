ebabe375736b6e37bbd5c9e5daa08017683b960a
jmigual <j.marce.igual@gmail.com>
2016-06-13 19:26:55 +0200

    Add dictionary documentation

--- Code/dictionary.h
+++ Code/dictionary.h
@@ -3,21 +3,50 @@
 #include <string>
 using namespace std;
 
-// Interface for all dictionaries
+/**
+ * @brief The Dictionary interface is used in all the dictionaries
+ */
 class Dictionary
 {
 public:
     
+    /// @brief Destructor
     virtual ~Dictionary() {}
 
-    virtual void insertElement(const string &) = 0;
+    /**
+     * @brief Insert the element into the dictionary
+     * @param elem Element to be inserted
+     */
+    virtual void insertElement(const string &elem) = 0;
 
-    virtual bool contains(const string &) = 0;
-
+    /**
+     * @brief Check if the element is in the dictionary
+     * @param elem Element to be checked
+     * @return <em>true</em> if the element is in the dictionary
+     * otherwise returns <em>false</em>
+     */
+    virtual bool contains(const string &elem) = 0;
+    
+    /**
+     * @brief reset Used in incremental search, resets the incremental search
+     */
     virtual void reset() = 0;
 
+    /**
+     * @brief Add one char in the dictionary incremental search and
+     * checks if the current generated word is in the dictionary
+     * @param c Character added to check if the formed word is in the dictionary
+     * @return <em>true</em> if the current generated sequence is in the 
+     * dictionary, otherwise returns <em>false</em>
+     */
     virtual bool stepForwards(char c) = 0;
     
+    /**
+     * @brief Remove the last character in the dictionary incremental search
+     * and check if it is possible to go backwards
+     * @return <em>true</em> if it is possible to go backwards in the current 
+     * state, otherwise returns <em>false</em>
+     */
     virtual bool stepBackwards() = 0;
 };
 
