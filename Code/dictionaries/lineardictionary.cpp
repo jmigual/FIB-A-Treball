e28a5bd1d6490d878e91afc0032df1f72e55e1ea
Kittus <markasenjo@hotmail.com>
2016-06-14 16:32:17 +0200

    no message

---
+++ Code/dictionaries/lineardictionary.cpp
@@ -0,0 +1,40 @@
+#include "lineardictionary.h"
+#include "algorithm"
+
+void LinearDictionary::insertElement(const string & S){
+    m_Dict.push_back(S);
+}
+
+bool LinearDictionary::contains(const string & S){
+    if (find(m_Dict.begin(), m_Dict.end(), S) != m_Dict.end()){
+        m_sCurrent = S;
+        return true;
+    }
+    return false;
+}
+
+void LinearDictionary::reset(){
+    m_sCurrent.clear();
+}
+
+bool LinearDictionary::stepForwards(char c){
+    string aux = m_sCurrent;
+    aux.push_back(c);
+
+    for (string s : m_Dict) {
+        auto res = mismatch(aux.begin(), aux.end(), s.begin(), s.end());
+        if (res.first == aux.end()) {
+            m_sCurrent.push_back(c);
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool LinearDictionary::stepBackwards(){
+    if (m_sCurrent.size() == 0) return false;
+    m_sCurrent.pop_back();
+    return true;
+}
+
