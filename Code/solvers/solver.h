ad3cda5536a410da076d4ae897783504b64b705a
jmigual <j.marce.igual@gmail.com>
2016-06-14 18:52:34 +0200

    Fixed bug in solver.h

--- Code/solvers/solver.h
+++ Code/solvers/solver.h
@@ -5,6 +5,7 @@
 #include <ostream>
 
 #include "dictionaries/dictionary.h"
+#include "board.h"
 #include "typedefs.h"
 
 class Solver
@@ -13,11 +14,13 @@ public:
     
     virtual void setDictionary(shared_ptr<Dictionary>) = 0;
     
-    virtual void setBoard(VVC &) = 0;
+    virtual void setWords(vector<string>&) = 0;
+    
+    virtual void setBoard(Board &) = 0;
     
     virtual void solve() = 0;
     
-    virtual void printSolution(std::ostream out) = 0;
+    virtual void printSolution(std::ostream &out) = 0;
 };
 
 #endif // SOLVER_H
